95a27158e453eb2cc8545dc0742b207099f0b23c
DmTs34 <34611261+DmTs34@users.noreply.github.com>
2019-08-05 15:29:13 +0200

    Update FOcablingselector.js

--- pages/FOcablingselector.js
+++ pages/FOcablingselector.js
@@ -288,6 +288,7 @@ this.ShowTrSelector = function(sideA){
         //when user clicks on icon - then the shape is selected and the FOselector re-draws
         //TODO add some animation effects here
         selTr.click(function (event,i){
+	console.log (i)
           if(sideA=="none"){
             let newTr = new Transceiver(i);
             newTr.Quantity=trQuantity[0];
