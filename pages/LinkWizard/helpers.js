aaaf4d5a8bd213b2214dfaa0c27b548a4e2e31cd
DmTs34 <34611261+DmTs34@users.noreply.github.com>
2023-08-18 08:27:44 +0200

    helpers updated

--- pages/LinkWizard/helpers.js
+++ pages/LinkWizard/helpers.js
@@ -51,76 +51,8 @@ function unhideTag(htmlTag) {
   })
 }
 
-//action logic for the "oninput" in the user form
-// function selectEvent(tag) {
-//   switch (tag.id) {
-//     case "dropdownTopology":
-//       if (tag.value === "001") { jsonData.linktype = "PL00" }
-//       if (tag.value === "004") { jsonData.linktype = "PE00" }
-//       jsonData.sides = "00_00"
-//       jsonData.permanentlink = "00_00_000_000"
-//       jsonData.portextension = "00_00_000_000"
-//       jsonData.topology = tag.value
-//       buildMenuLinkType();
-//       break;
-//     case "dropdownLinktype":
-//       jsonData.linktype = tag.value
-//       jsonData.sides = "00_00";
-//       jsonData.permanentlink = "00_00_000_000";
-//       jsonData.portextension = "00_00_000_000";
-//       buildMenuLinkType();
-//       break;
-//     case "dropdownSides":
-//       if (jsonData.linktype === "PL00") {
-//         if (tag.value === "00_00") { jsonData.permanentlink = "00_00_000_000"; }
-//         if (tag.value === "LC_LC") { jsonData.permanentlink = "Du_Du_000_000"; }
-//         if (tag.value === "MT_LC") { jsonData.permanentlink = "Pa_Du_000_000"; }
-//         if (tag.value === "MT_MT") { jsonData.permanentlink = "Pa_Pa_000_000"; }
-//         jsonData.portextension = "00_00_000_000";
-//       }
-//       if (jsonData.linktype === "IC00") {
-//         if (tag.value === "00_00") { jsonData.permanentlink = "00_00_000_000"; }
-//         if (tag.value === "Du_Du") { jsonData.permanentlink = "Du_Du_000_000"; }
-//         if (tag.value === "Pa_Du") { jsonData.permanentlink = "Pa_Du_000_000"; }
-//         if (tag.value === "Pa_Pa") { jsonData.permanentlink = "Pa_Pa_000_000"; }
-//         jsonData.portextension = "00_00_000_000";
-//       }
-//       if (jsonData.linktype === "PE00") {
-//         jsonData.permanentlink = "00_00_000_000";
-//         if (tag.value === "Du_LC") { jsonData.portextension = "Du_Du_000_000"; }
-//         if (tag.value === "Pa_LC") { jsonData.portextension = "Pa_Du_000_000"; }
-//         if (tag.value === "Pa_MT") { jsonData.portextension = "Pa_Pa_000_000"; }
-
-//       }
-//       if (jsonData.linktype === "CC00" || jsonData.linktype === "DC00") {
-//         jsonData.permanentlink = "00_00_000_000";
-//         jsonData.portextension = "00_00_000_000";
-//       }
-//       jsonData.sides = tag.value
-//       buildMenuSides();
-//       break;
-//     case "dropdownPermanentlink":
-//       if (jsonData.permanentlink.substring(0, 5) !== tag.value.substring(0, 5)) { jsonData.portextension = "00_00_000_000"; }
-//       if (tag.value === "00_00_000_000") { jsonData.portextension = "00_00_000_000"; }
-//       jsonData.permanentlink = tag.value
-//       buildMenuPermanentLink();
-//       break;
-//     case "dropdownpatchcordvariant":
-//       jsonData.patchcordvariant = tag.value
-//       break;
-//     case "dropdownPortextension":
-//       jsonData.portextension = tag.value
-//       buildMenuPortextension();
-//       break;
-
-//   }
 
 
-//   checkApplicationImage();
-//   checkChannelImage();
-//   addDataToURL();
-// }
-//adds one button
 //class="wButton" style="margin-left: 15px;"
 function addTransceiverButton(btnID, divID) {
   var btn = document.createElement("button");
@@ -135,13 +67,19 @@ function addTransceiverButton(btnID, divID) {
   div.appendChild(btn);
 }
 
-//adds one button
+function removeChildTags(divID){
+  var div = document.getElementById(divID);
+while (div.firstChild) {
+    div.removeChild(div.firstChild);
+}
+}
+//adds text to the paragraph
 function addParagraph(prText, prID, clean) {
   var pr = document.getElementById(prID);
   if (clean !== true) {
     var vocabularyData = jsonData.vocabulary[prText]
     if (vocabularyData === undefined) { vocabularyData = prText }
-    pr.textContent = pr.textContent + vocabularyData + " > "
+    pr.textContent = pr.textContent +" > "+ vocabularyData 
   } else {
     pr.textContent = "Your selection: "
   }
@@ -191,7 +129,7 @@ function buildButtonsArray(array, type, divID) {
     btnId = type + "=" + array[i]
     btn.setAttribute("id", btnId);
     btn.addEventListener("click", function () {
-      selectLength(btnId)
+      selectLength(this)
     })
     div.appendChild(btn);
   }
